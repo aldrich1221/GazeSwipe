bb495a07e6ea8773806d59de85dae69e5632d36f
aldrich1221 <30518471+aldrich1221@users.noreply.github.com>
2018-07-13 23:07:14 +0800

    Add files via upload

---
+++ Scenes/MyTouchInput.cs
@@ -0,0 +1,89 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MyTouchInput : MonoBehaviour
+{
+
+    public LayerMask touchInputMask;
+    private List<GameObject> touchlist = new List<GameObject>();
+    private GameObject[] touchesOld;
+    private RaycastHit hit;
+
+
+    public float speed = 0.1F;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("hhaa");
+        
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            // Get movement of the finger since last frame
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+
+            // Move object across XY plane
+            transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0);
+
+            Debug.Log(Input.GetTouch(0).position);
+        }
+           
+        /*
+      if (Input.touchCount > 0)
+      {
+
+          touchesOld = new GameObject[touchlist.Count];
+          touchlist.CopyTo(touchesOld);
+          touchlist.Clear();
+
+          foreach (Touch touch in Input.touches)
+          {
+              Ray ray = camera.ScreenPointToRay(touch.position);
+              RaycastHit hit;
+              if (physics.Raycast(ray, out hit, touchInputMask))
+              {
+                  GameObject recipient = hit.transform.gameobject;
+                  if (touch.phase == TouchPhase.began)
+                  {
+                      recipient.SendMessage("TouchDown", hit.point, SendMessageOptions.DontRequrieReceiver);
+                  }
+                  if (touch.phase == TouchPhase.Ended)
+                  {
+                      recipient.SendMessage("TouchUp", hit.point, SendMessageOptions.DontRequrieReceiver);
+                  }
+                  if (touch.phase == TouchPhase.Stationary || tocuh.phase == TouchPhase.Moved)
+                  {
+                      recipient.SendMessage("TouchStay", hit.point, SendMessageOptions.DontRequrieReceiver);
+                  }
+                  if (touch.phase == TouchPhase.Canceled)
+                  {
+                      recipient.SendMessage("TouchExit", hit.point, SendMessageOptions.DontRequrieReceiver);
+                  }
+
+              }
+          }
+
+          foreach (GameObject g in touchesOld)
+          {
+              if (!touchlist.Contains(g))
+              {
+                  g.SendMessage("OnTouchExit", hit.point, SendMessageOptions.DontRequireReceiver);
+              }
+          }
+
+
+
+      }
+          */
+    }
+}
+
---
+++ Scenes/NewBehaviourScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public Color defaultColour;
+    public Color selectedColour;
+    private Material mat;
+
+  /*
+
+    void Start()
+    {
+        mat = GameObject.GetComponent<Renderer>().material.color;
+
+    }
+    */
+    void TouchUp()
+    {
+        mat.color= defaultColour;
+
+    }
+    void TouchDown()
+    {
+        mat.color = selectedColour; 
+
+    }
+    void TouchExit()
+    {
+        mat.color = defaultColour;
+
+    }
+    void TouchStay()
+    {
+        mat.color = selectedColour;
+
+    }
+
+}
